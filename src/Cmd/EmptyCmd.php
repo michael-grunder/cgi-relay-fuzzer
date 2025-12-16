9a5da39992cfc5e9c57c8bb6991ca591f85c8795
michael-grunder <michael.grunder@gmail.com>
2025-12-16 13:12:38 -0800

    Empty command abstraction

---
+++ src/Cmd/EmptyCmd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Mgrunder\Fuzzer\Cmd;
+
+require_once __DIR__ . '/' . '../../vendor/autoload.php';
+
+abstract class EmptyCmd extends Cmd {
+    public function args(): array {
+        return [];
+    }
+}
